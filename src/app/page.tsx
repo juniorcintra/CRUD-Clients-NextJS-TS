a8b4d1e5d2380b924a888f85151089a359ffcba6
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-17 20:44:12 -0300

    sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const handleSign = async () => {
     await signIn("google");
   };
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  const { data } = useSession();
+  console.log("🚀 ~ data:", data);
 
   return <button onClick={handleSign}>Login</button>;
 }
