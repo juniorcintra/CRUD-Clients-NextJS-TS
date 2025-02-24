cfec56be5b59f2de8b5bfd373e137f1a4b1a60d1
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-24 02:51:05 -0300

    modal de confirmação de deletar

--- src/components/DataClients.tsx
+++ src/components/DataClients.tsx
@@ -19,13 +19,23 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Search, Trash } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonComponent from "./CustomButton";
 import { InputField, InputIcon, InputRoot } from "./Input";
 import { Button } from "./ui/button";
 import { deleteClients } from "@/actions/deleteClients";
 import { Client } from "@/types";
 import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import CustomButton from "./CustomButton";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -41,6 +51,7 @@ export function DataTable<TData, TValue>({
   search,
 }: DataTableProps<TData, TValue>) {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [clientsToDelete, setClientsToDelete] = useState<string[]>([]);
   const [rowSelection, setRowSelection] = useState({});
   const { refresh } = useRouter();
 
@@ -67,104 +78,152 @@ export function DataTable<TData, TValue>({
     refresh();
   };
 
+  useEffect(() => {
+    const clientNames = table
+      .getFilteredSelectedRowModel()
+      .rows.map((row) => (row.original as Client).name);
+
+    setClientsToDelete(clientNames);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [table, table.getFilteredSelectedRowModel().rows]);
+
   return (
-    <div className="h-fit w-full space-y-4 rounded-md bg-[#18181B] p-6">
-      <div className="flex w-full items-center justify-between">
-        <InputRoot>
-          <InputField
-            type="text"
-            placeholder="Pesquise por nome ou email"
-            value={search}
-            onChange={(event) => setSearch(event.target.value)}
-          />
-          <InputIcon>
-            <Search />
-          </InputIcon>
-        </InputRoot>
-        {table.getFilteredSelectedRowModel().rows.length > 0 && (
-          <div className="h-9 max-w-[220px]">
-            <ButtonComponent type="button" variant="red" onClick={handleDelete}>
-              Excluir Selecionado
-              {table.getFilteredSelectedRowModel().rows.length > 1 ? "s" : ""}
-              <Trash />
-            </ButtonComponent>
-          </div>
-        )}
-      </div>
-      <div className="rounded-md border border-[#27272A]">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
+    <Dialog>
+      <div className="h-fit w-full space-y-4 rounded-md bg-[#18181B] p-6">
+        <div className="flex w-full items-center justify-between">
+          <InputRoot>
+            <InputField
+              type="text"
+              placeholder="Pesquise por nome ou email"
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
+            />
+            <InputIcon>
+              <Search />
+            </InputIcon>
+          </InputRoot>
+          {table.getFilteredSelectedRowModel().rows.length > 0 && (
+            <div className="h-9 max-w-[220px]">
+              <DialogTrigger asChild>
+                <ButtonComponent type="button" variant="red">
+                  Excluir Selecionado
+                  {table.getFilteredSelectedRowModel().rows.length > 1
+                    ? "s"
+                    : ""}
+                  <Trash />
+                </ButtonComponent>
+              </DialogTrigger>
+            </div>
+          )}
+        </div>
+        <div className="rounded-md border border-[#27272A]">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center text-white"
-                >
-                  Nenhum cliente cadastrado.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} de{" "}
-          {table.getFilteredRowModel().rows.length} linhas selecionadas.
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center text-white"
+                  >
+                    Nenhum cliente cadastrado.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Anterior
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Próxima
-          </Button>
+        <div className="flex items-center justify-between">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} de{" "}
+            {table.getFilteredRowModel().rows.length} linhas selecionadas.
+          </div>
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Anterior
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Próxima
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <DialogContent className="!rounded-2xl">
+        <DialogHeader>
+          <DialogTitle className="font-normal">
+            <span className="text-red-500">CUIDADO:</span> Você está prestes a
+            excluir {clientsToDelete.length} cliente
+            {clientsToDelete.length > 1 ? "s" : ""}
+          </DialogTitle>
+          <DialogDescription>
+            <div className="my-4 h-[2px] w-full bg-[#94A3B8]" />
+            Tem certeza de que deseja excluir permanentemente{" "}
+            {clientsToDelete.length > 1 ? "os clientes " : "o cliente "}
+            <span className="text-red-500">
+              {clientsToDelete.map((client) => `"${client}"`).join(", ")}
+            </span>
+            ? Esta ação não pode ser desfeita e todos os dados relacionados{" "}
+            {clientsToDelete.length > 1 ? "aos clientes " : "ao cliente "},
+            incluindo histórico de empréstimos e faturas, serão removidos
+            permanentemente.
+            <div className="my-4 h-[2px] w-full bg-[#94A3B8]" />
+            <div className="flex w-full justify-end gap-3">
+              <DialogClose asChild>
+                <CustomButton type="button" variant="gray">
+                  Cancelar
+                </CustomButton>
+              </DialogClose>
+              <CustomButton variant="red" type="button" onClick={handleDelete}>
+                <Trash />
+                Deletar
+              </CustomButton>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
